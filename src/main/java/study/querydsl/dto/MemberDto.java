34e9f25c93055a055f7a746b1237c070d4dfb660
kyeoungchan <wkc1002@naver.com>
2023-12-21 18:58:42 +0900

    study: Intermediate Grammar - Projection Returns DTO

---
+++ src/main/java/study/querydsl/dto/MemberDto.java
@@ -0,0 +1,13 @@
+package study.querydsl.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class MemberDto {
+    private String username;
+    private int age;
+}
