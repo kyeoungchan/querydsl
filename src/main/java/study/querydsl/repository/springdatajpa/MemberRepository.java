ca2f8ee7c96cc1e35448baaaa6bbb8fdb487b7fb
kyeoungchan <wkc1002@naver.com>
2024-01-07 17:32:33 +0900

    study: Business Application - Spring Data JPA and Querydsl - User Custom Repository

--- src/main/java/study/querydsl/repository/springdatajpa/MemberRepository.java
+++ src/main/java/study/querydsl/repository/springdatajpa/MemberRepository.java
@@ -1,9 +1,9 @@
-package study.querydsl.repository;
+package study.querydsl.repository.springdatajpa;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.querydsl.entity.Member;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
     List<Member> findByUsername(String username);
 }
