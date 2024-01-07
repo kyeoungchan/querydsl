6cfbd7b337d3ecefd337ee4c82aa40b58f44614e
kyeoungchan <wkc1002@naver.com>
2024-01-07 16:01:56 +0900

    study: Business Application - Spring Data JPA and Querydsl - create Spring Data JPA Repository

---
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -0,0 +1,37 @@
+package study.querydsl.repository;
+
+import jakarta.persistence.EntityManager;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+public class MemberRepositoryTest {
+
+    @Autowired
+    EntityManager em;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void basicTest() {
+        Member member = new Member("member1", 10);
+        memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(member.getId())
+                .orElseThrow(IllegalArgumentException::new);
+        assertThat(findMember).isEqualTo(member);
+
+        List<Member> result1 = memberRepository.findAll();
+        assertThat(result1).containsExactly(member);
+
+        List<Member> result2 = memberRepository.findByUsername(member.getUsername());
+        assertThat(result2).containsExactly(member);
+    }
+}
